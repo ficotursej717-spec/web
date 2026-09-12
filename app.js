0ef0dd772fbc15779662ced0ef6d675ed6c10414
Leonardo Gallardo <leogallardo97-bit@users.noreply.github.com>
2026-09-12 11:57:13 +0200

    Vincular perfil oficial de ResearchGate de Leonardo Gallardo Apolo

--- app.js
+++ app.js
@@ -87,7 +87,8 @@ const investigadoresData = {
         links: {
             orcid: 'https://orcid.org/0009-0008-9384-5729',
             scholar: 'https://scholar.google.es/citations?user=H2JP8uwAAAAJ&hl=es',
-            linkedin: 'https://www.linkedin.com/in/leonardo-gallardo-apolo-16ab78355/'
+            linkedin: 'https://www.linkedin.com/in/leonardo-gallardo-apolo-16ab78355/',
+            researchgate: 'https://www.researchgate.net/profile/Leonardo-Gallardo-Apolo'
         }
     },
     'investigador-3': {
