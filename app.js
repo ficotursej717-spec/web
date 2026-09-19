59e843540b23e5137c9406db0f746865b4b55f98
Leonardo Gallardo <leogallardo97-bit@users.noreply.github.com>
2026-09-19 09:51:44 +0200

    feat(scholar): add Google Scholar citations and metrics panel for Dra. Amalia Hidalgo

--- app.js
+++ app.js
@@ -366,6 +366,7 @@ const investigadoresData = {
             despacho: 'Facultad de Derecho y Ciencias Económicas y Empresariales, Universidad de Córdoba'
         },
         links: {
+            scholar: 'https://scholar.google.com/citations?user=nSNNPL8AAAAJ&hl=es',
             orcid: 'https://orcid.org/0000-0002-2764-6624'
         }
     },
