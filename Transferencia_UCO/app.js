a720df1f3315ff21bbae35cc7585114ec26e7b77
Leonardo Gallardo <leogallardo97-bit@users.noreply.github.com>
2026-09-19 09:57:34 +0200

    feat(scholar): add Google Scholar citations and metrics panel for Dr. Jose Antonio Fernandez-Gallardo

--- Transferencia_UCO/app.js
+++ Transferencia_UCO/app.js
@@ -290,6 +290,7 @@ const investigadoresData = {
             despacho: 'Facultad de Derecho y Ciencias Económicas y Empresariales, Universidad de Córdoba'
         },
         links: {
+            scholar: 'https://scholar.google.com/citations?user=_Qv0_jUAAAAJ&hl=es',
             orcid: 'https://orcid.org/0000-0002-3003-3909'
         }
     },
